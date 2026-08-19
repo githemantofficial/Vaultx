d64aaeaff8c74c6509e765fa8a72e27cc3add117
Hemant Sharma <scndid1820@gmail.com>
2026-08-20 01:25:18 +0530

    Initial commit

---
+++ src/main/java/com/vaultx/VaultxApplication.java
@@ -0,0 +1,13 @@
+package com.vaultx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultxApplication.class, args);
+	}
+
+}
